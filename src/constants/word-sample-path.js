7ad0068612d6f5b1baf28bf6489e1d543dd69db4
yeahyeahcore <kirill.kirya.v.1.2.5@mail.ru>
2022-02-03 20:24:03 +0300

    fix: file generator

--- src/constants/word-sample-path.js
+++ src/constants/word-sample-path.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { app } = require("electron");
 
-const WORD_SAMPLE_PATH = path.join(app.getAppPath(), "./assets/format.docx");
+const WORD_SAMPLE_PATH = path.join(app.getAppPath(), "../assets/format.docx");
 
 module.exports = {
   WORD_SAMPLE_PATH,
